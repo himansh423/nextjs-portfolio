62e6b22bc6003ab342c378dc6a72591da6a5f935
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-05-24 09:24:23 +0530

    created login and verify otp pages

---
+++ src/app/admin/auth/verify-otp/page.tsx
@@ -0,0 +1,32 @@
+
+import VerifyOtp from "@/components/Admin/VerifyOtp";
+import sideLines from "../../../../../public/sideLines.jpg";
+
+import Login from "@/components/Admin/Login";
+
+const page = () => {
+  return (
+    <div className="w-full min-h-screen flex justify-between">
+      <div
+        className="w-[30px] min-h-screen bg-repeat-y bg-top max-md:hidden"
+        style={{
+          backgroundImage: `url(${sideLines.src})`,
+          backgroundSize: "contain",
+        }}
+      ></div>
+      <div className="flex-1 flex flex-col overflow-hidden">
+       <VerifyOtp/>
+      </div>
+
+      <div
+        className="w-[30px] min-h-screen bg-repeat-y bg-top max-md:hidden"
+        style={{
+          backgroundImage: `url(${sideLines.src})`,
+          backgroundSize: "contain",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/Admin/VerifyOtp.tsx
+++ src/components/Admin/VerifyOtp.tsx
@@ -1,27 +1,27 @@
 import { borderColor, fontColor } from "@/library/constants/colors";
 import { racingSans } from "@/library/constants/fonts";
-import { ArrowRight, Key, KeyRound, MailIcon } from "lucide-react";
+import { ArrowRight, Key, KeyRound, Lock, MailIcon } from "lucide-react";
 
-const Login = () => {
+const VerifyOtp = () => {
   return (
     <div className="w-full h-screen flex flex-col gap-3 items-center justify-center">
       <div className="flex flex-col gap-1 items-center">
         <p
           className={`text-[40px] ${racingSans.className} ${fontColor.primary}`}
         >
-          Admin Login
+          Verify OTP
         </p>
-        <p>Sign in to your admin account</p>
+        <p>Enter the verification code sent to Admin email</p>
       </div>
       <form
         className={`w-[500px]  rounded-[10px] border-[1px] ${borderColor.primary} shadow-2xl flex flex-col px-5 py-4`}
       >
         <div>
           <p className={`text-[25px] font-bold ${fontColor.primary}`}>
-            Admin Login
+            Enter Verification Code
           </p>
           <p className={` text-[14px] ${fontColor.secondry}`}>
-            Enter your credentials to continue
+            We've sent a 6-digit code to Admin Registered Email
           </p>
         </div>
         <div className="mt-5 flex flex-col gap-2">
@@ -29,60 +29,36 @@ const Login = () => {
             htmlFor="email"
             className={`text-[16px] font-semibold ${fontColor.primary} `}
           >
-            Email
+            Verification Code
           </label>
           <div
             className={`h-[40px] w-full flex items-center justify-center border-[1px] ${borderColor.primary} rounded-[5px]`}
           >
             <div className="w-[60px] h-full  flex items-center justify-center ">
-              <MailIcon className={`${fontColor.secondry}`} />
+              <Lock className={`${fontColor.secondry}`} />
             </div>
 
             <input
-              type="email"
-              id="email"
-              className={`flex-1 h-full  px-2 focus:outline-none`}
-              placeholder="admin@example.com"
+              type="text"
+              id="otp"
+              className={`flex-1 h-full  px-2 focus:outline-none  text-[14px]`}
+              placeholder="1 2 3 4 5 6"
             />
           </div>
         </div>
-        <div className="mt-5 flex flex-col gap-2">
-          <label
-            htmlFor="email"
-            className={`text-[16px] font-semibold ${fontColor.primary} `}
-          >
-            Password
-          </label>
-          <div
-            className={`h-[40px] w-full flex items-center justify-center border-[1px] ${borderColor.primary} rounded-[5px]`}
-          >
-            <div className="w-[60px] h-full  flex items-center justify-center ">
-              <KeyRound className={`${fontColor.secondry}`} />
-            </div>
-
-            <input
-              type="password"
-              id="password"
-              className={`flex-1 h-full  px-2 focus:outline-none`}
-              placeholder="********"
-            />
-          </div>
-        </div>
+        
         <button
           className={`w-full h-[40px] bg-black flex items-center justify-center text-[#fafafa] mt-5 rounded-[5px] gap-1 text-[14px] font-semibold`}
         >
-          <p>Sign In</p>
+          <p>Verify Code</p>
           <div>
             <ArrowRight className="mt-[2px] " size={17} />
           </div>
         </button>
-        <div className="w-full mt-5 h-[60px] flex gap-1 items-start justify-center">
-          <p>Forgot Password?</p>
-          <span className="text-blue-500"> click here</span>
-        </div>
+       
       </form>
     </div>
   );
 };
 
-export default Login;
+export default VerifyOtp;
